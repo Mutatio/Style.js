1a3bc82263aad66a817552bcd11af781205eee8c
Martin Gallagher <martin@martinpaulgallagher.com>
2012-06-20 19:58:42 +0100

    Add version constants and methods.

--- src/jss.hpp
+++ src/jss.hpp
@@ -15,6 +15,15 @@ extern char _binary_JSS_js_start[];
 extern char _binary_JSS_js_end[];
 
 namespace jss {
+	// Embedded JSS version
+	static const short JSS_MAJOR_VERSION = 0;
+	static const short JSS_MINOR_VERSION = 0;
+	static const short JSS_PATCH_VERSION = 1;
+
+	// jssc program version
+	static const short JSSC_MAJOR_VERSION = 1;
+	static const short JSSC_MINOR_VERSION = 0;
+	static const short JSSC_PATCH_VERSION = 0;
 
 	/**
 	 * @return Copy of embedded JSS JavaScript
@@ -29,6 +38,39 @@ namespace jss {
 		}
 
 		return ss.str();
+	}
+
+	/**
+	 * @return Formatted string output of JSS version
+	 */
+	static string getJSSVersion() {
+		stringstream in;
+		string out;
+
+		in << JSS_MAJOR_VERSION << "." << JSS_MINOR_VERSION << "." << JSS_PATCH_VERSION;
+		in >> out;
+
+		return out;
+	}
+
+	/**
+	 * @return Formatted string output of JSSC version
+	 */
+	static string getJSSCVersion() {
+		stringstream in;
+		string out;
+
+		in << JSSC_MAJOR_VERSION << "." << JSSC_MINOR_VERSION << "." << JSSC_PATCH_VERSION;
+		in >> out;
+
+		return out;
+	}
+
+	/**
+	 * @return Formatted string output of JSS, JSSC and Google V8 versions
+	 */
+	static string getVersionString() {
+		return "-------------------\nComponent | Version\n-------------------\nJSS       | " + getJSSVersion() + "\nJSSC      | " + getJSSCVersion() + "\nGoogle V8 | " + v8::V8::GetVersion();
 	}
 
 	class Compiler {
